098b07324da284d21d663e5bb0d84d0165b23c34
pablogarpas <pablogp696@gmail.com>
2018-01-25 18:29:10 +0100

    Cambiada la forma de enlazar etiquetas y alertas

--- basic/views/alerta-etiquetas/_form.php
+++ basic/views/alerta-etiquetas/_form.php
@@ -1,23 +1,45 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
 use yii\widgets\ActiveForm;
+use app\models\AlertaEtiquetas;
+use app\models\Alerta;
+use app\models\Etiquetas;
+use app\models\AlertaEtiquetasSearch;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\AlertaEtiquetas */
 /* @var $form yii\widgets\ActiveForm */
+
+$alertas = Alerta::find()->all();
+$etiquetas = Etiquetas::find()->all();
+
+$alertalista=ArrayHelper::map($alertas,'id','titulo');
+
+$etiquetalista=ArrayHelper::map($etiquetas,'id','nombre');
 ?>
 
-<div class="alerta-etiquetas-form">
+<div class="categorias-etiquetas-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'alerta_id')->textInput(['maxlength' => true]) ?>
+    <!-- <?= $form->field($model, 'alerta_id')->textInput() ?>
 
-    <?= $form->field($model, 'etiqueta_id')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'etiqueta_id')->textInput() ?> -->
+
+    <?= $form->field($model, 'alerta_id')->dropdownList(
+			$alertalista,
+	    	['prompt'=>'Selecciona una alerta']);
+	?>
+
+   	<?= $form->field($model, 'etiqueta_id')->dropdownList(
+		$etiquetalista,
+	    	['prompt'=>'Selecciona una etiqueta']);
+	?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Crear') : Yii::t('app', 'Actualizar'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
