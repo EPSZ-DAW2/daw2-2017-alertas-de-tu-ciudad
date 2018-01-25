daf342e8fb2976bcdd2ccc2e4c1c64c1d1fbb280
DoxCode <31673020+DoxCode@users.noreply.github.com>
2018-01-25 18:51:56 +0100

    Implementada la opción de modificar imagenes.
    Implementada la edición de imagenes, que permitirá al usuario que ha creado una alerta, modificar las imagenes que ha subido.
    
    Está pensado para que en caso de ser administrador tengas opciones adicionales de edición, pudiendo editar cualquier imagen, sea tuya o no, poder cambiar a que alerta pertenece y por quién o cuando fue creada.
    
    La edición de imagenes guardará la imagen en la misma ruta que estaba originalmente, retirando la anterior primero.

---
+++ basic/views/alerta-imagenes/update_admin_form.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Parametros adicionales que tendrá el administrador a la hora de modificar 
+ * las imagenes.
+**/
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\AlertaImagen */
+/* @var $form yii\widgets\ActiveForm */
+
+/* PERMITIREMOS a los administradores:
+ * Poder cambiar la imagen a otra alerta.
+ * Poder cambiar el usuario y la fecha en que se subió.
+ * Agregar una nota de administración.
+ * 
+ * --- De forma automática:
+ * Se seleccionará automáticamente la fecha de modificación así como la id del modificador.
+ * El orden de la imagen prevalecerá.
+ * 
+ *  */
+?>
+
+    <?= $form->field($model, 'alerta_id')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'crea_usuario_id')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'crea_fecha')->textInput() ?>
+
+    <?= $form->field($model, 'notas_admin')->textarea(['rows' => 6]) ?>
+
+</div>
