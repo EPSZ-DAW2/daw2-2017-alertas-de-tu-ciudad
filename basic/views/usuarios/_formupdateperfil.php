d23f784cc4b6fde0bf47e875e50f2adf61ff1516
AleOspina15 <35371917+AleOspina15@users.noreply.github.com>
2018-01-26 19:21:11 +0100

    Perfil de usuario conectado

---
+++ basic/views/usuarios/_formupdateperfil.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Usuario */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="usuario-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'nick')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'apellidos')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'fecha_nacimiento')->textInput() ?>
+
+    <?= $form->field($model, 'direccion')->textarea(['rows' => 6]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Guardar cambios', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ basic/views/usuarios/perfil.php
@@ -0,0 +1,52 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Usuario */
+
+$this->title = $model->nick;
+$this->params['breadcrumbs'][] = ['label' => 'Usuarios', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="usuario-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Modificar', ['updateperfil', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Eliminar', ['usuario-incidencias/createconsulta'], [
+            'class' => 'btn btn-danger',
+            
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            //'id',
+            'email:email',
+            'password',
+            'nick',
+            'nombre',
+            'apellidos',
+            'fecha_nacimiento',
+            'direccion:ntext',
+           /* 'area_id',
+            'rol',
+            'fecha_registro',
+            'confirmado',
+            'fecha_acceso',
+            'num_accesos',
+            'bloqueado',
+            'bloqueo_usuario_id',
+            'bloqueo_fecha',
+            'bloqueo_notas:ntext',*/
+        ],
+    ]) ?>
+	<p>
+		<?= Html::a(Yii::t('app', 'Crear una incidencia'), ['usuario-incidencias/createconsulta'], ['class' => 'btn btn-success']) ?>
+		<?= Html::a(Yii::t('app', 'Ver mis incidencias'), ['usuario-incidencias/index', 'id' => $model->id], ['class' => 'btn btn-success']) ?>
+	</p>
+</div>
