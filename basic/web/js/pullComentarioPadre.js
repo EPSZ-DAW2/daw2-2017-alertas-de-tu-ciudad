63f76a5d3723f10a3c04c092545afac68710f7af
Iván Carrasco <ivancarras@usal.es>
2018-01-28 20:19:20 +0100

    Creación del comentario respondido a través de AJAX

---
+++ basic/web/js/pullComentarioPadre.js
@@ -0,0 +1,10 @@
+function pullComentarioPadre(idPadre){
+    $.ajax({
+        jQuery.ajax({
+        url:'//alerta-comentarios/actionobtenerinfopadre?id=107',
+        type:'POST',
+        success:function() {
+
+        }
+    });
+}
