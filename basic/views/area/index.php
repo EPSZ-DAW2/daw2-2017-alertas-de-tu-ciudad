f05e4e96fb9d1c14f9b270fd233a64b364ac104a
carlos gonzález <cgrs@cgrs.me>
2018-01-30 19:15:50 +0100

    Cambio en la administración de las áreas
    - Ahora se entra a la administración por la acción admin
    - La vista principal está oculta en el menú, pero es accesible a los usuarios normales
    - Sólo los administradores podrán entrar en admin

---
+++ basic/views/area/index.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\AreaSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = Yii::t('app', 'Áreas');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="area-admin">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a(Yii::t('app', 'Create Area'), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <? Pjax::begin(); ?>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            ['attribute' => 'claseArea', 'filter' => $clasesArea ],
+            'nombre',
+            'parentName',
+            'totalAlertas',
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+    <? Pjax::end(); ?>
+</div>
