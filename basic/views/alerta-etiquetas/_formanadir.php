1573b208b42451e40fe19c3f46d4377c0ac6efff
pablogarpas <pablogp696@gmail.com>
2018-01-31 21:28:29 +0100

    Ahora se cogerá automaticamente la alerta al añadir etiquetas

---
+++ basic/views/alerta-etiquetas/_formanadir.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
+use yii\widgets\ActiveForm;
+use app\models\AlertaEtiquetas;
+use app\models\Alerta;
+use app\models\Etiquetas;
+use app\models\AlertaEtiquetasSearch;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\AlertaEtiquetas */
+/* @var $form yii\widgets\ActiveForm */
+
+$alertas = Alerta::find()->all();
+$etiquetas = Etiquetas::find()->all();
+
+$alertalista=ArrayHelper::map($alertas,'id','titulo');
+
+$etiquetalista=ArrayHelper::map($etiquetas,'id','nombre');
+?>
+<div class="categorias-etiquetas-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <!-- <?= $form->field($model, 'alerta_id')->textInput() ?>
+
+    <?= $form->field($model, 'etiqueta_id')->textInput() ?> -->
+
+   	<?= $form->field($model, 'etiqueta_id')->dropdownList(
+		$etiquetalista,
+	    	['prompt'=>'Selecciona una etiqueta']);
+	?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Crear') : Yii::t('app', 'Actualizar'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
